4c23f4ecdb98548e21b45b9cc4f6dc2ecdb2cd59
unknown <ian.battistoni.f@gmail.com>
2025-07-02 02:27:37 -0400

    imgs

--- src/shared/layouts/header/header.tsx
+++ src/shared/layouts/header/header.tsx
@@ -1,11 +1,14 @@
+
 import './header.css';
+//assets
+
 
 function Header(){
     return(
-        <header>
-            <nav>
+        <header className="header">
+            <nav className="nav">
                 <a href='/' title='Ir a inicio'>Inicio</a>
-                <a href='/about' title='Ir a inicio'>Sobre nostros</a>
+                <a href='/about' title='Ir a inicio'>Nostros</a>
             </nav>
         </header>
     );

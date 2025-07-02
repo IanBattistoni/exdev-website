fae8582ef019acc19cfafecbd43b215fe2c8b41a
unknown <ian.battistoni.f@gmail.com>
2025-07-02 04:44:10 -0400

    save

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,27 @@
 import './home.css';
-
+import logo1 from '../../assets/img/IMG_9483.webp';
+import logo2 from '../../assets/img/IMG_9468.webp';
 function Home() {
     return(
-        <p>hello world</p>
+        <section className='home-page'>
+            <section className='home-intro'>
+            <section className='presentation'>
+                <div className='text'>
+                    <h1 className='fade-in-left'>Club de Desarrollo Experimental ExDev</h1>
+                    <h3 className='fade-in-right'>Somos un club que une los conceptos <strong>experimentar</strong> y <strong>desarrollar</strong>.
+                    </h3>
+                </div>
+                
+            </section>
+        <section className='gallery fade-in-bottom delay-2'>
+            <img src={logo1} alt='img1' />
+            <img src={logo2} alt='img2' />
+            <img src={logo1} alt='img1-duplicate' />
+            <img src={logo2} alt='img2-duplicate' />
+      </section>
+      </section>
+        </section>
+        
     );
     
 }
--- src/shared/layouts/header/header.tsx
+++ src/shared/layouts/header/header.tsx
@@ -1,12 +1,21 @@
 
 import './header.css';
 //assets
-
-
+import ExdevLogo from '../../../assets/img/exdevlogo.png';
+import ExdevSoloLogo from '../../../assets/img/exdev-solologo.png';
 function Header(){
     return(
         <header className="header">
+            <div className="logo-container">
+                <a href="/">
+                <picture>
+                    <source srcSet={ExdevSoloLogo} media="(max-width: 364px)" />
+                    <img src={ExdevLogo} alt="Logo Exdev" className="exdev-logo" />
+                </picture>
+                </a>
+            </div>
             <nav className="nav">
+                
                 <a href='/' title='Ir a inicio'>Inicio</a>
                 <a href='/about' title='Ir a inicio'>Nostros</a>
             </nav>

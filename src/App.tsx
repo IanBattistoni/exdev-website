ae3f4df86c577067e200ae20c85f50db34cca763
unknown <ian.battistoni.f@gmail.com>
2025-07-13 03:55:37 -0400

    manifest, apply route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home'
 import About from './pages/about/about'
-
+import Apply from './pages/apply/apply';
 function App() {
   return (
     <Router>
@@ -11,6 +11,7 @@ function App() {
 
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/apply" element={<Apply />} />
 
         <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
       </Routes>
---
+++ src/pages/apply/apply.tsx
@@ -0,0 +1,13 @@
+import './apply.css';
+
+function Apply() {
+    return(
+        <section className="apply-page">
+        <h1>Unete</h1>
+
+        </section>
+    );
+    
+}
+
+export default Apply;
